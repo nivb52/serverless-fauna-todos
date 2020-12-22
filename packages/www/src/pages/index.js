a72a20650ca9b3c9cf59cacef3d835d2fbbe6a81
niv b <nivb52@gmail.com>
2020-12-22 23:49:20 +0200

    auth

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Flex, Heading, Button } from 'theme-ui';
-export default (props) => (
-  <Container>
-    <Flex sx={{ flexDirection: 'column', padding: 3 }}>
-      <Heading as="h1"> Getting Staff Done</Heading>
-      <Button sx={{ marginTop: 2 }}>Log In</Button>
-    </Flex>
-  </Container>
-);
+// import netlifyIdentity from 'netlify-identity-widget';
+import auth from '../services/iam';
+
+export default (props) => {
+  useEffect(() => {
+    auth.init({});
+  });
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: 'column', padding: 3 }}>
+        <Heading as="h1"> Getting Staff Done</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            auth.open();
+          }}>
+          Log In
+        </Button>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            auth.currentUser();
+          }}>
+          current user
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
