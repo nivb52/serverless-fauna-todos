ad19d19ee73425b76adf1c7d35c43242ce9029d4
niv b <nivb52@gmail.com>
2021-01-16 23:32:36 +0200

    add useUser Hook

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container, Flex, Heading } from 'theme-ui';
 import Menu from '../cmps/Menu';
+import useUser from '../hooks/useUser';
 
 export default (props) => {
+  const fullName = useUser();
   return (
     <Container>
       <Flex sx={{ flexDirection: 'column', padding: 3 }}>
-        <Menu></Menu>
+        <Menu isUser={!!fullName} fullName={fullName}></Menu>
         <Heading as="h1" sx={{ textAlign: 'center' }}>
           Getting Staff Done
         </Heading>
